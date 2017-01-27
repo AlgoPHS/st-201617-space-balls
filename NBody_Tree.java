3d6d4bef034e19190e1fc4ed53e3dd8297d0a4f5
Brendan DeMilt <bdemilt98@gmail.com>
2017-01-27 11:51:57 -0500

    Quad tree for nbody simulation

--- NBody_Tree.java
+++ NBody_Tree.java
@@ -24,17 +24,22 @@ public class NBody_Tree {
 			dr = null;
 			info = null;
 		}
-		
-		
-		public void drawQuad(){
+
+		public void drawQuad() {
 			StdDraw.setPenColor(StdDraw.BLACK);
-			StdDraw.square((space.getX()[0]+space.getX()[1])/2, (space.getY()[0]+space.getY()[1])/2, (space.getX()[1]-space.getX()[0])/2);
+			StdDraw.square((space.getX()[0] + space.getX()[1]) / 2, (space.getY()[0] + space.getY()[1]) / 2,
+					(space.getX()[1] - space.getX()[0]) / 2);
 			StdDraw.point(info.getPos()[0], info.getPos()[1]);
-			
+
 		}
-		
-		
-		
+
+		// splits the node into its many many children
+		public void initChildren() {
+			ul = new Node(new Square(space.getX()[0], (space.getY()[0] + space.getY()[1]) / 2, space.getLen() / 2));
+			ur = new Node(new Square((space.getX()[0] + space.getX()[1]) / 2, (space.getY()[0] + space.getY()[1]) / 2,space.getLen() / 2));
+			dl = new Node(new Square(space.getX()[0], space.getY()[0], space.getLen() / 2));
+			dr = new Node(new Square((space.getX()[0] + space.getX()[1]) / 2, space.getY()[0], space.getLen() / 2));
+		}
 
 		public double getMass() {
 			if (info != null)
@@ -51,18 +56,16 @@ public class NBody_Tree {
 
 	// root quadrant
 	private Node root;
-	
-	
-	
-	//this ratio is used to see if a body's force should be used to determine if a sole planet's force should be
-	//taken or if the center of mass of the general area.
+
+	// this ratio is used to see if a body's force should be used to determine
+	// if a sole planet's force should be
+	// taken or if the center of mass of the general area.
 	// if theta ==1, it will be most accurate but will be brute force
 	// if theta ==0, it will be the least accurate but be quickest
 	private double theta = .5;
-	
-	
-	
-//initializes universe with first quadrant being having side lengths equal to the scale
+
+	// initializes universe with first quadrant being having side lengths equal
+	// to the scale
 	public NBody_Tree(double scale) {
 		root = new Node(new Square(0, 0, scale));
 	}
@@ -73,29 +76,27 @@ public class NBody_Tree {
 
 	private void add(Node n, Planet b) {
 
-	
-		
-		
-		
-		
 	}
 
-	
-	//determines if a node is internal, where it is the parent to many different planets
-	private boolean isInternalNode(Node x){
-		
-		if(isPlanet(x))return false;
-		else return internal(x.ur) || internal(x.dr) || internal(x.dl) || internal(x.ul);
-		
+	// determines if a node is internal, where it is the parent to many
+	// different planets
+	private boolean isInternalNode(Node x) {
+
+		if (isPlanet(x))
+			return false;
+		else
+			return internal(x.ur) || internal(x.dr) || internal(x.dl) || internal(x.ul);
+
 	}
-	
-	//recursive function that traverses the quadtree
-	private boolean internal(Node x){
-		if(isPlanet(x))return true;
-		else return internal(x.ur) || internal(x.dr) || internal(x.dl) || internal(x.ul);
+
+	// recursive function that traverses the quadtree
+	private boolean internal(Node x) {
+		if (isPlanet(x))
+			return true;
+		else
+			return internal(x.ur) || internal(x.dr) || internal(x.dl) || internal(x.ul);
 	}
-	
-	
+
 	// If a node has no children AND has a non-null planet class, it has a
 	private boolean isPlanet(Node n) {
 		if (n == null)
@@ -103,23 +104,13 @@ public class NBody_Tree {
 		else
 			return n.ul == null && n.ur == null && n.dl == null && n.dr == null && n.info != null;
 	}
-	
-	
-	
-	
-	public static void main(String[] args){
-		
-		
-		
-		
+
+	public static void main(String[] args) {
+
 		
 		
 		
 		
 	}
-	
-	
-	
-	
 
 }
