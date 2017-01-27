64fbee6a9f8be9e8fff95ad299f9ebdcf80512be
Brendan DeMilt <bdemilt98@gmail.com>
2017-01-27 12:16:30 -0500

    Implemented most of what we need for initializing the tree

--- NBody_Tree.java
+++ NBody_Tree.java
@@ -29,6 +29,7 @@ public class NBody_Tree {
 			StdDraw.setPenColor(StdDraw.BLACK);
 			StdDraw.square((space.getX()[0] + space.getX()[1]) / 2, (space.getY()[0] + space.getY()[1]) / 2,
 					(space.getX()[1] - space.getX()[0]) / 2);
+			if(info != null)
 			StdDraw.point(info.getPos()[0], info.getPos()[1]);
 
 		}
@@ -63,29 +64,42 @@ public class NBody_Tree {
 	// if theta ==1, it will be most accurate but will be brute force
 	// if theta ==0, it will be the least accurate but be quickest
 	private double theta = .5;
-<<<<<<< HEAD
 
 	// initializes universe with first quadrant being having side lengths equal
 	// to the scale
-=======
-	
-	
-	
-	//initializes universe with first quadrant being having side lengths equal to the scale
->>>>>>> 70b408c8ed111588150d98c76d3d7686bee859bb
 	public NBody_Tree(double scale) {
 		root = new Node(new Square(0, 0, scale));
 	}
 
-	public void addBody(Planet p) {
-		add(root, p);
-	}
-
-<<<<<<< HEAD
+	
 	private void add(Node n, Planet b) {
-
-	}
-
+        if(isInternalNode(n))
+        {
+        	
+        	n.info.updateCenterOfMass(b);
+        	
+        	if(b.isIn(n.ur.space)) add(n.ur , b);
+        	if(b.isIn(n.ul.space)) add(n.ul , b);
+        	if(b.isIn(n.dr.space)) add(n.dr , b);
+        	if(b.isIn(n.dl.space)) add(n.dl , b);
+        }
+          
+        else if(isPlanet(n))
+        {
+            Planet p = n.info;
+            n.initChildren();
+           
+            if(p.isIn(n.ur.space)) n.ur.setPlanet(p);
+        	if(p.isIn(n.ul.space)) n.ul.setPlanet(p);
+        	if(p.isIn(n.dr.space)) n.dr.setPlanet(p);
+        	if(p.isIn(n.dl.space)) n.dl.setPlanet(p);
+            add(n , b);
+        }
+        else
+        {
+            n.setPlanet(b);
+        }
+    }
 	// determines if a node is internal, where it is the parent to many
 	// different planets
 	private boolean isInternalNode(Node x) {
@@ -95,15 +109,6 @@ public class NBody_Tree {
 		else
 			return internal(x.ur) || internal(x.dr) || internal(x.dl) || internal(x.ul);
 
-=======
-	
-	//determines if a node is internal, where it is the parent to many different planets
-	private boolean isInternalNode(Node x){
-		
-		if(isPlanet(x))return false;
-		else return internal(x.ur) || internal(x.dr) || internal(x.dl) || internal(x.ul);
-		
->>>>>>> 70b408c8ed111588150d98c76d3d7686bee859bb
 	}
 
 	// recursive function that traverses the quadtree
@@ -121,63 +126,9 @@ public class NBody_Tree {
 		else
 			return n.ul == null && n.ur == null && n.dl == null && n.dr == null && n.info != null;
 	}
-<<<<<<< HEAD
 
 	public static void main(String[] args) {
 
-=======
-	
-	
-	private void add(Node n, Planet b) {
-        if(isInternalNode(n))
-        {
-            double nodex , nodey, squarex, squarey;
-            nodex = b.getPos()[0];
-            nodey = b.getPos()[1];
-            squarex = (n.space.getX()[1] + n.space.getX()[0]) / 2;
-            squarey = (n.space.getY()[1] + n.space.getY()[0]) / 2;
-            if(nodex > squarex)
-            {
-                if(nodey > squarey)
-                {
-                    add(n.ur , b);
-                }
-                else
-                {
-                    add(n.dr , b);
-                }
-            }
-            else
-            {
-                if(nodey < squarey)
-                {
-                    add(n.ul , b);
-                }
-                else
-                {
-                    add(n.dl , b);
-                }
-            }
-        }
-        else if(isPlanet(n))
-        {
-            Planet p = n.info;
-            n.setPlanet(null);
-            add(n , p);
-            add(n , b);
-        }
-        else
-        {
-            n.setPlanet(b);
-        }
-    }
-	
-	public static void main(String[] args){
-		
-		
-		
-		
->>>>>>> 70b408c8ed111588150d98c76d3d7686bee859bb
 		
 		
 		
